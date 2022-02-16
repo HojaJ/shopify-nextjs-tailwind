1c59a891759c8bab080b748fcfa1c843efbdc202
HojaJ <hojajepbar@gmail.com>
2022-02-16 19:24:31 +0500

    First Commit

---
+++ components/ProductContentPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+export default function ProducContentPage({ product }) {
+    
+    return (
+        <div>
+            { product.title }
+        </div>
+    )
+
+}
+
